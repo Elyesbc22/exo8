ccdb8b058a5ae723268a2749e009a553e7dc0820
elyesbc22 <elyesbc22@gmail.com>
2025-07-14 10:38:46 +0200

    exo8

---
+++ acme/tools/lecture.py
@@ -0,0 +1,11 @@
+"""
+TO DO
+
+Développer un outil capable de parcourir un dossier et de lister les fichiers et les sous-dossiers qu'il contient.
+L'outil doit afficher la structure du dossier et indiquer le nombre de fichiers et de sous-dossiers à chaque niveau.
+ doit être utiliser dans main.py
+
+"""
+
+def parcourir_dossier(dossier):
+    return 0
